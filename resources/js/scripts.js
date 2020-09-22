dd248ba9518705751ab95982594256362326ce64
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2020-09-22 14:21:51 -0700

    got the images to display

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -3,49 +3,80 @@ import data from './data.js';
 
 // the number of items in the array will determine the times it will loop through
 
-for(let i = 0; i< data.length; i++){
-
+data.forEach(function (object, index){
+    // creates a new div element with the class name "item"
     let newDiv = document.createElement("div");
     newDiv.className = "item";
 
+    // creates a img tag
     let img = document.createElement("img");
-
-    img.src = data[i].image;
+    // goes through each index called object and gives it these attributes
+    img.src = object.image;
     img.width = 300;
     img.height = 300;
-
+    // add it to the div tag
     newDiv.appendChild(img);
-    console.log(img)
-    itemsContainer.appendChild(newDiv);
+    document.querySelector('#items').appendChild(newDiv)
+    console.log(img);
 
-    let desc = document.createElement("P");
-    desc.innerText = data[i].desc;
-    newDiv.appendChild(desc);
+    // create a p tag
+    let desc = document.createElement("p");
+    desc.innerText = object.desc; // grabs the value for the key description
+    newDiv.appendChild(desc); // adds it to the div tag
+    console.log(desc);
 
-    let price = document.createElement("P");
-    price.innerText = data[i].price;
-    newDiv.appendChild(price);
+     // create a p tag
+    let price = document.createElement("p");
+    price.innerText = object.price; // grabs the value for the key price
+    newDiv.appendChild(price); // adds it to the div tag
+    console.log(price)
 
-    let button = document.createElement("button");
-    button.id = data[i].name;
-    button.dataset.price = data[i].price;
-    button.innerHTML = "Add to Cart";
-    newDiv.appendChild(button);
+    let button = document.createElement("button"); // create a button
+    button.id = object.name;  // grabs the value for the key name
+    button.dataset.price = object.price;
+    button.innerHTML = "Add to Cart"; // name of button
+    newDiv.appendChild(button); // add it to the div tag
+    console.log(button)
     
-}
+});
 
 const cart = [ ];
 
 function addItem(name, price){
+    for(let i = 0; i < cart.length; i += 1){
+        if(cart[i].name === name){
+            cart[i].qty += 1
+            return
+        }
+    }
     const item = {name: name, price: price, qty: 1}
 cart.push(item)
 }
 
 function showItems(){
-    console.log( `you have ${cart.length} items in your cart`)
+    let qty = 0;
+    for(let i = 0; i < cart.length; i += 1){
+        qty += cart[i].qty;
+    }
+    console.log( `you have ${qty} items in your cart`);
+    
+    for(let i = 0; i < cart.length; i += 1){
+        console.log(`$${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
+    }
+
+    let total = 0;
+    for(let i = 0; i < cart.length; i += 1){
+        total += cart[i].price * cart[i].qty
+    }
+
+    console.log(`Total in cart: $${total.toFixed(2)}`)
 }
 
 addItem('Apple', 0.99);
 addItem('Orange', 1.29);
+addItem('Apple', 0.99);
+addItem('Orange', 1.29);
+addItem('Grapes', 5.99);
+
 showItems();
 
